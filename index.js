af0001247ff476a76a6cafa5845c0d5780152938
Louis Klein <louisklein71@gmail.com>
2023-05-02 21:24:30 -0700

    test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+    if(record.find(r => r.result === 'W')){
+     return record.find(r => r.result === 'W').year
+    }
+    else{
+        return record.find(r => r.result === 'W')
+    }
+}
+
+
